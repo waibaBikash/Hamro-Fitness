b6c0056bf4f7684afc33e0029bb56a3b976bdf94
waibaBikash <kavrelibikash@gmail.com>
2026-04-08 19:57:50 +1000

    feat: enhance AppContext with user state management and food/activity logs

--- client/src/context/AppContext.tsx
+++ client/src/context/AppContext.tsx
@@ -1,4 +1,35 @@
-import { createContext } from "react";
-import { initialState } from "../types";
+import { createContext, useContext, useState } from "react";
+import { initialState, type ActivityEntry, type FoodEntry, type User } from "../types";
+import { useNavigate } from "react-router-dom";
 
 const AppContext = createContext(initialState);
+
+
+export const AppProvider = ({ children } : { children: React.ReactNode })=> {
+
+  const navigate = useNavigate();
+  const [user, setUser] = useState<User>(null);
+  const [isUserFetched, setIsUserFetched] = useState(false);
+  const [onboardingCompleted, setOnboardingCompleted] = useState(false);
+  const [allFoodLogs, setAllFoodLogs] = useState<FoodEntry[]>([]);
+  const [allActivityLogs, setAllActivityLogs] = useState<ActivityEntry[]>([]);
+
+  const value = {
+    user,
+    setUser,
+    isUserFetched,
+    setIsUserFetched,
+    onboardingCompleted,
+    setOnboardingCompleted,
+    allFoodLogs,
+    setAllFoodLogs,
+    allActivityLogs,
+    setAllActivityLogs
+  };
+  return <AppContext.Provider value={value}>
+   {children}
+  </AppContext.Provider>
+}
+
+
+export const useAppContext = () => useContext(AppContext);
