7bd96efe9e27175a7d7925d0908987ec0138cc7d
waibaBikash <kavrelibikash@gmail.com>
2026-04-17 19:11:22 +1000

    feat: enhance Login component with username input field for signup

--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -1,3 +1,4 @@
+import { AtSignIcon } from "lucide-react"
 import { useState } from "react"
 
 
@@ -14,6 +15,19 @@ const Login = () => {
                  {state === 'login' ? 'Sign In' : 'Sign Up'}
                </h2>
                <p className="text-sm text-gray-500/90 dark:text-gray-400">{state === 'login' ? 'Welcome back! Please enter your details.' : 'Create an account to get started.'}</p>
+
+               {state !== 'login' && (
+                 <div className="mt-4">
+                    <label className="font-medium text-sm text-gray-700 dark:text-gray-300" >
+                      Username</label>
+                      <div className="relative mt-2">
+                         <AtSignIcon 
+                         className="absolute left-3 top-1/2 -translate-y-1/2 text-gray-400 size-4.5"
+                         />
+                         <input type="text" />
+                      </div>
+                 </div>
+               )}
              </form>
         </main>
     </>
