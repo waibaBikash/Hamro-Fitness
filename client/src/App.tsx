9c5fba484e268ca1c32404497cd69634ef47a7ae
waibaBikash <kavrelibikash@gmail.com>
2026-04-21 19:50:43 +1000

    feat: add onboarding check to App component for user flow management

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -7,6 +7,7 @@ import Profile from "./pages/Profile"
 import { useAppContext } from "./context/AppContext"
 import Login from "./pages/Login"
 import Loading from "./components/Loading"
+import Onboarding from "./pages/Onboarding"
 
 
 const App = () => {
@@ -14,6 +15,9 @@ const App = () => {
   const { user , isUserFetched, onboardingCompleted } =  useAppContext()
     if(!user){
       return isUserFetched ? <Login /> : <Loading />
+    }
+    if (!onboardingCompleted) {
+      return <Onboarding />
     }
   return (
     <>
